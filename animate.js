73ae161fa2adb792a84636f4eac6cf67add107ce
Jubayer Hossain <83045122+Jubayer-Hossain-JB@users.noreply.github.com>
2023-10-17 13:27:56 +0600

    Update animate.js

--- animate.js
+++ animate.js
@@ -1,10 +1,7 @@
-gsap.from('.top',{duration:1, y:'-200%', ease: "bounce.out"});
-gsap.fromTo('.time', {rotation:600,scale:0}, {duration:3, delay:1, rotation:0, scale:1});
-gsap.from('.daw',{duration:2, delay:2, opacity:0});
-gsap.from('.Welcome',{duration:2, x:"-100vw", delay:1.5, ease: "elastic.out"});
-gsap.from('.det', {duration:2, x: '100vw', delay: 1.5});
-gsap.from('.month', {delay: 1,duration: 2,scale:0, opacity:0, ease: 'bounce.out'})
-gsap.from('.mname', {delay: 1, duration: 2, x: "-50%", opacity: 0})
+gsap.from('.top',{duration:0, opacity:0, ease: "back.out(1.4)"});
+gsap.from('.daw',{duration:2, scale: 0.6,opacity:0, ease:"back.out(3)"});
+gsap.from('.Welcome',{duration:1, y:"5vw", scaleY: 0.2, opacity:0,ease: "back.out(1.4)"});
+gsap.from('.det', {duration:1, delay: 0.5 ,y:"5vw", scaleY: 0.2, opacity:0,ease: "back.out(1.4)"});
 
 $(document).ready(function(){
     $('.top').click(function(){
@@ -13,19 +10,19 @@ $(document).ready(function(){
         },1000);
     });
     
-    $(window).scroll(function(e){
-        var scroll = $(window).scrollTop();
-        if ($('.daw').css('height') > 100){
-            $('.daw').animate({
-            height: (200 - (scroll*2)),
-            width: (120 - scroll),
-            'background-size': (133-scroll),
-        }, 'fast')
-        }else{
-            $('.daw').animate({
-                'position': 'sticky',
-                'top': scroll-20,
-            },200)
-        }
-    })
+    // $(window).scroll(function(e){
+    //     var scroll = $(window).scrollTop();
+    //     if ($('.daw').css('height') > 100){
+    //         $('.daw').animate({
+    //         height: (200 - (scroll*2)),
+    //         width: (120 - scroll),
+    //         'background-size': (133-scroll),
+    //     }, 'fast')
+    //     }else{
+    //         $('.daw').animate({
+    //             'position': 'sticky',
+    //             'top': scroll-20,
+    //         },200)
+    //     }
+    // })
 })
