d7fda1d0e1b11b7f5079785bacdc7f18af607990
Jubayer Hossain <83045122+Jubayer-Hossain-JB@users.noreply.github.com>
2023-10-17 13:34:14 +0600

    Update animate.js

--- animate.js
+++ animate.js
@@ -2,6 +2,8 @@ gsap.from('.top',{duration:0, opacity:0, ease: "back.out(1.4)"});
 gsap.from('.daw',{duration:2, scale: 0.6,opacity:0, ease:"back.out(3)"});
 gsap.from('.Welcome',{duration:1, y:"5vw", scaleY: 0.2, opacity:0,ease: "back.out(1.4)"});
 gsap.from('.det', {duration:1, delay: 0.5 ,y:"5vw", scaleY: 0.2, opacity:0,ease: "back.out(1.4)"});
+// gsap.from('.mgroup div', {duration:1, delay: 0.5 , "--blury": "0px", ease: "back.out(1.4)"});
+// gsap.from('.mname', {delay: 1, duration: 2, x: "-50%", opacity: 0})
 
 $(document).ready(function(){
     $('.top').click(function(){
@@ -10,19 +12,19 @@ $(document).ready(function(){
         },1000);
     });
     
-    // $(window).scroll(function(e){
-    //     var scroll = $(window).scrollTop();
-    //     if ($('.daw').css('height') > 100){
-    //         $('.daw').animate({
-    //         height: (200 - (scroll*2)),
-    //         width: (120 - scroll),
-    //         'background-size': (133-scroll),
-    //     }, 'fast')
-    //     }else{
-    //         $('.daw').animate({
-    //             'position': 'sticky',
-    //             'top': scroll-20,
-    //         },200)
-    //     }
-    // })
+//     $(window).scroll(function(e){
+//         var scroll = $(window).scrollTop();
+//         if ($('.daw').css('height') > 100){
+//             $('.daw').animate({
+//             height: (200 - (scroll*2)),
+//             width: (120 - scroll),
+//             'background-size': (133-scroll),
+//         }, 'fast')
+//         }else{
+//             $('.daw').animate({
+//                 'position': 'sticky',
+//                 'top': scroll-20,
+//             },200)
+//         }
+//     })
 })
