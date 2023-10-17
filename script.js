cdbbf8bd8ce6e9e628127f04e20e49e4ecba8479
Jubayer Hossain <83045122+Jubayer-Hossain-JB@users.noreply.github.com>
2023-10-17 13:42:34 +0600

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function setDate(add){//mark today
 		store = today;
 		$(today).addClass('today')
 		monthArrey =[".month.1",".month.2",".month.3",".month.4",".month.5",".month.6",".month.7",".month.8",".month.9",".month.10",".month.11",".month.12",]
-		$('html, body').animate({scrollTop : 2},800,
+		$('html, body').animate({scrollTop : 2},0,
 			function(){
 				setTimeout(
 				function(){$('html, body').animate({scrollTop : $(monthArrey[mon]).position()['top']-180},800)},
